3710470b98b4dccfb00ad24e38ba0f28643d95f1
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-04 10:44:49 -0500

    finished Movie Ratings App

--- Proj22_01_MovieRatings/src/business/MovieCollection.java
+++ Proj22_01_MovieRatings/src/business/MovieCollection.java
@@ -1,11 +1,49 @@
 package business;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class MovieCollection {
+	private List<Movie> movies = new ArrayList<>();
 	
-	public class Movie {
-		 private String title;
-		 private int year;
-		 private double rating;
+	// add Movie object to internal list
+	public void add(Movie m) {
+		movies.add(m);
+	}
+	public List<Movie> filterMovies(Predicate<Movie> condition) {
+		List<Movie> filteredMovies = new ArrayList<>();
+		for (Movie m: movies) {
+			if (condition.test(m)) {
+				filteredMovies.add(m);
+			}
 		}
+		return filteredMovies;
+	}
+	public double getLowestRating() {
+		double min = movies.stream()
+				.map(m -> m.getRating())
+				.reduce(5.0, (a,b) -> Math.min(a, b));
+		return min;
+	}
+	public double getHighestRating() {
+		double max = movies.stream()
+				.map(m -> m.getRating())
+				.reduce(0.0, (a,b) -> Math.max(a, b));
+		return max;
+	}
+	public double getAverageRating() {
+		double avg = (movies.stream().mapToDouble(
+				m -> m.getRating()).average()).getAsDouble();
+		return avg;
+	}
+	//return number of items in internal list
+	public int getSize() {
+		return movies.size();
+	}
+	
+	public List<Movie> getMovies() {
+		return movies;
+	}
 	
 }
