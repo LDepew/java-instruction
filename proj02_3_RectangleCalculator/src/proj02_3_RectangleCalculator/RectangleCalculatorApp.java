58ee0bc1e4f9f9f857c7efbc5f642d02d012d842
Lance <MetalDeathMusic@yahoo.com>
2021-04-29 15:59:57 -0400

    Create rectangle calculator app

---
+++ proj02_3_RectangleCalculator/src/proj02_3_RectangleCalculator/RectangleCalculatorApp.java
@@ -0,0 +1,38 @@
+package proj02_3_RectangleCalculator;
+
+import java.util.Scanner;
+
+public class RectangleCalculatorApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Area and Perimeter Calculator");
+		System.out.println(); //blank line
+		System.out.print("Enter length: ");
+		double length = sc.nextInt();
+		System.out.print("Enter width: ");
+		double width = sc.nextInt();
+		System.out.println("Area: " + length * width);
+		System.out.println("Perimeter: " + (2 * length) + (2 * width));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye");
+
+	}
+
+}
