e14bb8a20669b4b34267631fe3aee2780a5d41dd
Lance <MetalDeathMusic@yahoo.com>
2021-04-29 16:09:58 -0400

    final retangle calculator

--- proj02_3_RectangleCalculator/src/proj02_3_RectangleCalculator/RectangleCalculatorApp.java
+++ proj02_3_RectangleCalculator/src/proj02_3_RectangleCalculator/RectangleCalculatorApp.java
@@ -6,29 +6,22 @@ public class RectangleCalculatorApp {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) { 
+			
 		System.out.println("Welcome to the Area and Perimeter Calculator");
 		System.out.println(); //blank line
 		System.out.print("Enter length: ");
-		double length = sc.nextInt();
+		double length = sc.nextDouble();
 		System.out.print("Enter width: ");
-		double width = sc.nextInt();
+		double width = sc.nextDouble();
 		System.out.println("Area: " + length * width);
 		System.out.println("Perimeter: " + (2 * length) + (2 * width));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(); //blank line
+		System.out.print("Continue? (y/n): ");
+		choice = sc.next();
+		}
 		
 		
 		System.out.println("Bye");
