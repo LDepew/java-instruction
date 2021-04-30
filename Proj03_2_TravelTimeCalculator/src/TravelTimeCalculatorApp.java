76929711207f6bddf501570a901326e4b1830d3b
Lance <Metal_Death_Music@yahoo.com>
2021-04-30 16:02:47 -0400

    init commit Travel Time Calculator

--- Proj03_2_TravelTimeCalculator/src/TravelTimeCalculatorApp.java
+++ Proj03_2_TravelTimeCalculator/src/TravelTimeCalculatorApp.java
@@ -4,15 +4,6 @@ public class TravelTimeCalculatorApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To The Travel Time Calculator");
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter miles: ");
-		double Miles = sc.nextDouble();
-		System.out.print("Enter miles per hour: ");
-		double MilesPerHour = sc.nextDouble();
-		System.out.println(); //blank line space
-		System.out.println("Estimated Travel Time");
-		System.out.println("---------------------");
-		System.out.println("Time " + Miles / MilesPerHour);
 		
 		
 		
