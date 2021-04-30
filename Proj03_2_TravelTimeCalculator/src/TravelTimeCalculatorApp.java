07e72116af76e7ee927abb5c9362bec49476fedf
Lance <MetalDeathMusic@yahoo.com>
2021-04-30 15:42:56 -0400

    Init commit Travel Time Calculator

---
+++ Proj03_2_TravelTimeCalculator/src/TravelTimeCalculatorApp.java
@@ -0,0 +1,22 @@
+
+public class TravelTimeCalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Travel Time Calculator");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye");
+
+	}
+
+}
