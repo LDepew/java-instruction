16c8c7d1765e22fd0328724c31a87fa031b2eb2a
Lance <Metal_Death_Music@yahoo.com>
2021-05-04 14:51:13 -0400

    init commit Interest Calculator

---
+++ Proj03_3_InterestCalculator/src/InterestCalculatorApp.java
@@ -0,0 +1,19 @@
+
+public class InterestCalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Interest Calculator");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye");
+	}
+
+}
