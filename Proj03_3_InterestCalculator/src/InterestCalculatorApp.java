b9f921fe75d9217c9e14152f85ff935473c86870
Lance <Metal_Death_Music@yahoo.com>
2021-05-04 14:55:35 -0400

    working Interest Calculator

--- Proj03_3_InterestCalculator/src/InterestCalculatorApp.java
+++ Proj03_3_InterestCalculator/src/InterestCalculatorApp.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class InterestCalculatorApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Interest Calculator");
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter loan amount: ");
+		double loanamt = sc.nextDouble();
+		System.out.print("Enter interest amount: ");
+		double interestamt = sc.nextDouble();
+		System.out.println(); //blank line space
+		System.out.println("Loan amount: " + loanamt);
+		System.out.println("Interest amount: " + interestamt);
+		
 		
 		
 		
