fb14d55ee011d700672ef096de3d6ecfda37c9d8
Lance <Metal_Death_Music@yahoo.com>
2021-05-13 14:13:26 -0400

    Logic commit Table of Powers

--- Proj04_1_TableOfPowers/src/TableOfPowersApp.java
+++ Proj04_1_TableOfPowers/src/TableOfPowersApp.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class TableOfPowersApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Squares and Cubes table");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter an integer: ");
+		int int1 = sc.nextInt();
+		
+		for (int i=1; i<= int1; i++) {
+			System.out.println(i);
+		}
+		
 
 		
 		
