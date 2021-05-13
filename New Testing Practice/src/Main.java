789ad68e6af2543cc20ac1ea1c845e583373b5c5
Lance <Metal_Death_Music@yahoo.com>
2021-05-13 13:47:42 -0400

    Init commit Table of Powers

--- New Testing Practice/src/Main.java
+++ New Testing Practice/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
-	}
-
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 }
---
+++ Proj04_1_TableOfPowers/src/TableOfPowersApp.java
@@ -0,0 +1,21 @@
+
+public class TableOfPowersApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Squares and Cubes table");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye");
+	}
+
+}
