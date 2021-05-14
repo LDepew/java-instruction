04ca9a28f4f1ccdd2eee8fba69e1f9074dc42e79
Lance <Metal_Death_Music@yahoo.com>
2021-05-14 14:18:59 -0400

    Init commit Factorial Calculator

---
+++ Proj04_2_FactorialCalculator/src/FactorialCalculatorApp.java
@@ -0,0 +1,25 @@
+
+public class FactorialCalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Factorial Calculator");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Bye");
+
+	}
+
+}
