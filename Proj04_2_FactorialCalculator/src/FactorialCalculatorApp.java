8dc3a612203b4999f28fa679e26f4fdf59c86fb3
Lance <Metal_Death_Music@yahoo.com>
2021-05-14 14:48:31 -0400

    working Factorial Calculator

--- Proj04_2_FactorialCalculator/src/FactorialCalculatorApp.java
+++ Proj04_2_FactorialCalculator/src/FactorialCalculatorApp.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 
 public class FactorialCalculatorApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Factorial Calculator");
 		
+		String choice = "y";
+		while(choice.equalsIgnoreCase("y")) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter and integer: ");
+		int input = sc.nextInt();
+		long fact = 1;
+		
+		for (int i=1; i<= input; i++) {
+			fact = fact * i;
+		}
+		System.out.println("The factorial of " +input+ " is " +fact);
 		
 		
 		
@@ -22,4 +35,5 @@ public class FactorialCalculatorApp {
 
 	}
 
+}
 }
