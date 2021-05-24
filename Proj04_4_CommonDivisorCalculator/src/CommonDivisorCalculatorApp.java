a79f53f37beee815a79c459b1dc71e06d07aadb1
Lance <Metal_Death_Music@yahoo.com>
2021-05-24 15:50:11 -0400

    update Common Divisor Calculator

--- Proj04_4_CommonDivisorCalculator/src/CommonDivisorCalculatorApp.java
+++ Proj04_4_CommonDivisorCalculator/src/CommonDivisorCalculatorApp.java
@@ -6,6 +6,19 @@ public class CommonDivisorCalculatorApp {
 		System.out.println("Greatest Common Divisor Finder");
 		
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first number:");
+		int x = sc.nextInt();
+		System.out.print("Enter second number:");
+		int y = sc.nextInt();
+		
+		while (x != 0) {
+			while (x > y) {
+				
+			}
+		}
+		
+		System.out.println(x + " & " + y);
 
 	}
 
