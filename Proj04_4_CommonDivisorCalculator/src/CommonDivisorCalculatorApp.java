393797e5df8625d96026a4d6052b186c9e86a35a
Lance <Metal_Death_Music@yahoo.com>
2021-05-24 14:45:41 -0400

    Init commit Common Divisor Calculator

---
+++ Proj04_4_CommonDivisorCalculator/src/CommonDivisorCalculatorApp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CommonDivisorCalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Greatest Common Divisor Finder");
+		
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
