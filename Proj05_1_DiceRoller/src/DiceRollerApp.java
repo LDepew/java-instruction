034b49d12e729cdd5046d462a26be8000e5962d1
Lance <Metal_Death_Music@yahoo.com>
2021-05-25 14:15:54 -0400

    Snake Eyes Dice Roller

--- Proj05_1_DiceRoller/src/DiceRollerApp.java
+++ Proj05_1_DiceRoller/src/DiceRollerApp.java
@@ -6,9 +6,11 @@ public class DiceRollerApp {
 		System.out.println("Dice Roller");
 		System.out.println(); //blank line space
 		
+		Scanner sc = new Scanner(System.in);
 		String choice = "y";
 		
-		Scanner sc = new Scanner(System.in);
+		
+		while(choice.equalsIgnoreCase("y")) {
 		
 		System.out.print("Roll the dice? (y/n): ");
 		choice = sc.next();
@@ -20,7 +22,20 @@ public class DiceRollerApp {
 		System.out.println("Die 1: " + die1);
 		System.out.println("Die 2: " + die2);
 		System.out.println("Total: " + (die1 + die2));
-
+		
+		if (die1 == 1 & die2 == 1) {
+			System.out.println("Snake Eyes!");
+		}
+		if (die1 == 6 && die2 == 6) {
+			System.out.println("Box Car!");
+		}
+		
+		}
+		System.out.print("Roll again? (y/n): ");
+		choice = sc.next();
+		
+		System.out.println(); //blank line space
+		System.out.println("Bye");
 	}
 
 }
