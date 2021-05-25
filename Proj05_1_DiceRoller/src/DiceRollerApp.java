558fd8eb52b3ede370ff1f1b4fd313a993f03a1f
Lance <Metal_Death_Music@yahoo.com>
2021-05-25 13:38:38 -0400

    Init commit Dice Roller

---
+++ Proj05_1_DiceRoller/src/DiceRollerApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DiceRollerApp {
+
+	public static void main(String[] args) {
+		System.out.println("Dice Roller");
+		System.out.println(); //blank line space
+		
+		String choice = "y";
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Roll the dice? (y/n): ");
+		choice = sc.next();
+		
+		int die1 = (int) ((Math.random() * 6) + 1);
+		int die2 = (int) ((Math.random() * 6) + 1);
+		
+		System.out.println(die1 + " " + die2);
+
+	}
+
+}
